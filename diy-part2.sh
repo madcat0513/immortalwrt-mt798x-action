78640171bd0463d83403ca25a22944e4645aa020
madcat0513 <horny1979@126.com>
2025-08-09 22:35:47 +0800

    add bpf&nikki
    增加了nikki和dae的依赖包，还有wireguard

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,6 +9,31 @@
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 #
+# add kmod-xdp-sockets-diag
+echo '
+
+define KernelPackage/xdp-sockets-diag
+  SUBMENU:=$(NETWORK_SUPPORT_MENU)
+  TITLE:=PF_XDP sockets monitoring interface support for ss utility
+  KCONFIG:= \
+	CONFIG_XDP_SOCKETS=y \
+	CONFIG_XDP_SOCKETS_DIAG
+  FILES:=$(LINUX_DIR)/net/xdp/xsk_diag.ko
+  AUTOLOAD:=$(call AutoLoad,31,xsk_diag)
+endef
+
+define KernelPackage/xdp-sockets-diag/description
+ Support for PF_XDP sockets monitoring interface used by the ss tool
+endef
+
+$(eval $(call KernelPackage,xdp-sockets-diag))
+' >> package/kernel/linux/modules/netsupport.mk
+
+
+echo '
+
+CONFIG_PACKAGE_kmod-xdp-sockets-diag=y
+' >> .config
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
